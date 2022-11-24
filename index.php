efbaccd2d77654db53f7a62f49b7cf98986fd3d8
Divya Rajasekar <39696006+divya-run@users.noreply.github.com>
2022-11-24 12:21:19 +0400

    tag

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
 <?php ?>
 <head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-P2HHGPFGMS"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-P2HHGPFGMS');
+</script>
 <link rel="stylesheet" href="index.css" />
 </head>
 <body>
    <div class=container>
    Hey cutie, Happy Happy Birthday! <br><br>
-<img src="reshab.jpeg" style="width:100%">
-  <br> <br>
+<img src="reshab.jpeg" style="width:100%"><br><br>
+  
   Reshab..my baby..<br>
   You changed my world baby, you make me soo happy! You are my sunshine :) You deserve all the happiness in this world! <br> Wish I could just leave everything and come hug you right now.  
    </div>
   <br>
-  You expected this to be a full on website no?:D <br>
-  For more info, please visit on 22/11/2023. 
+You expected this to be a full on website no?:D <br>
+  For more info, please visit on 22/11/2023.
   
 </body>
   
